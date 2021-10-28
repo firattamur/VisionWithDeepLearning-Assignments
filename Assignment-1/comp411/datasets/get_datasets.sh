576006006e527a12d82993fd7a5dc3f44b6006cb
firattamur <ftamur16@ku.edu.tr>
2021-10-28 18:22:04 +0300

    feat: three-layer-net hyperparameter tuning

--- Assignment-1/comp411/datasets/get_datasets.sh
+++ Assignment-1/comp411/datasets/get_datasets.sh
@@ -1,4 +1,4 @@
 # Get CIFAR10
-wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz
+wget https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz --no-check-certificate
 tar -xzvf cifar-10-python.tar.gz
 rm cifar-10-python.tar.gz 
